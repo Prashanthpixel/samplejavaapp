5de592ca91b1a6a227458727c1b4aac12b6c5591
Prashanthpixel <131933796+Prashanthpixel@users.noreply.github.com>
2023-07-18 18:51:25 +0530

    Update CaseInsensitiveComparator.java

--- src/main/java/com/devopsdemo/utilities/CaseInsensitiveComparator.java
+++ src/main/java/com/devopsdemo/utilities/CaseInsensitiveComparator.java
@@ -14,7 +14,7 @@ public class CaseInsensitiveComparator extends GenericComparator {
         /*
          * This functiona call base GenericComparator(boolean sortAscending) class and set whether sorting is in ascending or descending
          * sortAscending = true then ascending
-         * sortAscending = false then descending  
+         * sortAscending = false then descending
          */
         public CaseInsensitiveComparator(boolean sortAscending) {
                 super(sortAscending);
